e2362be11b095afdfb56aaefbc113c339eb4f57a
BaileyHodgson <baileyhodgson3@gmail.com>
2023-05-26 15:50:43 +0100

    Fixed data_loader to work with new dataframe approach

--- data_class.py
+++ data_class.py
@@ -6,13 +6,26 @@ import pandas as pd
 import matplotlib as plt
 import pathlib
 
+# Arguemnts gather input and output directorys.
 parser = argparse.ArgumentParser()
-parser.add_argument("--root", type=str, nargs="?", help="root")
-parser.add_argument("--target", type=str, nargs="?", help="target")
+parser.add_argument(
+    "--input",
+    type=str,
+    nargs="?",
+    help="input files of unconditioned data, please provide root directory.",
+)
+parser.add_argument(
+    "--output",
+    type=str,
+    nargs="?",
+    help="output files of conditioned data, please provide root directory, cannot be within input root.",
+)
 args = parser.parse_args()
 
 
 def find_max_tier(df, row):
+    # This function should, given a dataframe and a row, find all rows in that database with matching details, 
+    # Then return the value of the highest iteration in that dataframe
     result = df.query(
         "Plane == '{}' and VG == '{}' and VD == '{}' and Location == '{}' and Type == '{}' and Criteria == '{}' and Height == '{}' and Width == '{}'".format(
             row["Plane"],
@@ -28,54 +41,53 @@ def find_max_tier(df, row):
 
     return result.max()["Iteration"]
 
+
 def rreplace(s, old, new, occurrence):
+    # This function will replace a part of a string, searching from right to left instead.
     li = s.rsplit(old, occurrence)
     return new.join(li)
 
 
 class Aggregator:
     def __init__(self, args):
-        self.root = args.root
-        self.target = args.target
+        self.root = args.input
+        self.target = args.output
 
         self.df = (
             pd.DataFrame()
-        )  # [plane, vg, vd, location, type, iter, crit, width, height, path]
+        )       # [plane, vg, vd, location, type, iter, criteria, width, height, path]
         self.df_cond = (
             pd.DataFrame()
-        )  # [plane, vg, vd, location, type, iter, crit, width, height, path, mean, std, cmpPath, tarPath]
+        )       # [plane, vg, vd, location, criteria, height, widt, inpPotPath, inpChargePath, 
+                # cmpPotPath, cmpChargePath, tarPotPath, tarChargePath, Pot Mean, Pot STD, Charge Mean, Charge STD]
 
     def save(self, filename, condFlag=0):
+        # This function saves the dataframe to CSV, condFlag indicates which dataframe will be saved.
         try:
             os.mkdir(join(self.target))
         except Exception as e:
-            print("Found target dir")
+            print("Located Target Directory")
 
         if condFlag == 0:
             self.df.to_csv(join(self.target, filename))
-            print("Saved Dataframe to .csv!")
         else:
             self.df_cond.to_csv(join(self.target, filename))
 
     def read(self):
-        # getting all files from all subdirectories and paths to said files
-        print("Reading Files...")
+        # Getting all files from all subdirectories and paths to said files by performing a walk.
+        # Creates a dataframe using relevant files and directory names.
         imgs = []
         name_as_lst = []
-        VGnums = set()
-        VDnums = set()
         target_prefix = "NEGF"
 
-        filenum = 0
-        filenames = []
         for root, dirs, files in os.walk(self.root):
             for name in files:
                 if name.startswith(target_prefix):
                     path = os.path.join(root, name)
                     name = str(name).split(".txt")[0]  # Removing .txt at end of strings
                     name_as_lst = name.split("_")  # Spliting to
-                    XY = name_as_lst.pop(0)[-2:]
-                    name_as_lst.insert(0, XY)
+                    plane = name_as_lst.pop(0)[-2:]
+                    name_as_lst.insert(0, plane)
                     Criteria = str(root).split("/")[-3]
                     Shape = str(root).split("/")[-2].split("x")
                     Height = Shape[0]
@@ -103,9 +115,9 @@ class Aggregator:
     def condition(self):
         # Condition should go through the entire self.array and condition based on the iteration number and data type
         # Get all entries with iterator == 1, each entire to find max and mid values of same result
+
         dataList = []
         unique_devices = self.df.query("Iteration == '1' and Type == 'pot'")
-        # [plane, vg, vd, location, type, iter, crit, width, height, path]
 
         try:
             os.mkdir(join(self.target))
@@ -113,7 +125,6 @@ class Aggregator:
             print("Found target dir")
 
         for index, row in unique_devices.iterrows():
-
             tmpList = row.to_list()
             tmpList.pop(5)
             tmpList.pop(4)
@@ -127,15 +138,15 @@ class Aggregator:
 
             tarPotPath = self.df.query(
                 "Plane == '{}' and VG == '{}' and VD == '{}' and Location == '{}' and Type == '{}' and Iteration == '{}' and Criteria == '{}' and Height == '{}' and Width == '{}'".format(
-                row["Plane"],
-                row["VG"],
-                row["VD"],
-                row["Location"],
-                row["Type"],
-                tar,
-                row["Criteria"],
-                row["Height"],
-                row["Width"],
+                    row["Plane"],
+                    row["VG"],
+                    row["VD"],
+                    row["Location"],
+                    row["Type"],
+                    tar,
+                    row["Criteria"],
+                    row["Height"],
+                    row["Width"],
                 )
             ).iloc[0]["Path"]
 
@@ -143,21 +154,21 @@ class Aggregator:
 
             cmpPotPath = self.df.query(
                 "Plane == '{}' and VG == '{}' and VD == '{}' and Location == '{}' and Type == '{}' and Iteration == '{}' and Criteria == '{}' and Height == '{}' and Width == '{}'".format(
-                row["Plane"],
-                row["VG"],
-                row["VD"],
-                row["Location"],
-                row["Type"],
-                cmp,
-                row["Criteria"],
-                row["Height"],
-                row["Width"],
+                    row["Plane"],
+                    row["VG"],
+                    row["VD"],
+                    row["Location"],
+                    row["Type"],
+                    cmp,
+                    row["Criteria"],
+                    row["Height"],
+                    row["Width"],
                 )
             ).iloc[0]["Path"]
 
             cmpChargePath = rreplace(cmpPotPath, "pot", "charge", 1)
 
-            norms = [[0,0],[0,0]]
+            norms = [[0, 0], [0, 0]]
 
             norms[0][0] = (np.loadtxt(inpPotPath)).mean()
             norms[0][1] = (np.loadtxt(inpPotPath)).std()
@@ -165,29 +176,47 @@ class Aggregator:
             norms[1][0] = (np.log10(np.loadtxt(inpChargePath))).mean()
             norms[1][1] = (np.log10(np.loadtxt(inpChargePath))).std()
 
-            tmpPath = inpPotPath.split('/')[-4:]
+            tmpPath = inpPotPath.split("/")[-4:]
             deviceDir = join(self.target, tmpPath[0], tmpPath[1], tmpPath[2])
-            path = pathlib.Path(join(deviceDir,'tmp.txt'))
+            path = pathlib.Path(join(deviceDir, "tmp.txt"))
             path.parent.mkdir(parents=True, exist_ok=True)
 
-            np.savetxt(join(self.target, deviceDir, inpPotPath.split('/')[-1]), (np.loadtxt(inpPotPath) - norms[0][0]) / (norms[0][1]))
-            tmpList.append(join(self.target, deviceDir, inpPotPath.split('/')[-1]))
+            np.savetxt(
+                join(self.target, deviceDir, inpPotPath.split("/")[-1]),
+                (np.loadtxt(inpPotPath) - norms[0][0]) / (norms[0][1]),
+            )
+            tmpList.append(join(self.target, deviceDir, inpPotPath.split("/")[-1]))
 
-            np.savetxt(join(self.target, deviceDir, inpChargePath.split('/')[-1]), (np.log10(np.loadtxt(inpChargePath)) - norms[1][0]) / (norms[1][1]))
-            tmpList.append(join(self.target, deviceDir, inpChargePath.split('/')[-1]))
+            np.savetxt(
+                join(self.target, deviceDir, inpChargePath.split("/")[-1]),
+                (np.log10(np.loadtxt(inpChargePath)) - norms[1][0]) / (norms[1][1]),
+            )
+            tmpList.append(join(self.target, deviceDir, inpChargePath.split("/")[-1]))
 
-            np.savetxt(join(self.target, deviceDir, cmpPotPath.split('/')[-1]), (np.loadtxt(cmpPotPath) - norms[0][0]) / (norms[0][1]))
-            tmpList.append(join(self.target, deviceDir, cmpPotPath.split('/')[-1]))
+            np.savetxt(
+                join(self.target, deviceDir, cmpPotPath.split("/")[-1]),
+                (np.loadtxt(cmpPotPath) - norms[0][0]) / (norms[0][1]),
+            )
+            tmpList.append(join(self.target, deviceDir, cmpPotPath.split("/")[-1]))
 
-            np.savetxt(join(self.target, deviceDir, cmpChargePath.split('/')[-1]), (np.log10(np.loadtxt(cmpChargePath)) - norms[1][0]) / (norms[1][1]))
-            tmpList.append(join(self.target, deviceDir, cmpChargePath.split('/')[-1]))
+            np.savetxt(
+                join(self.target, deviceDir, cmpChargePath.split("/")[-1]),
+                (np.log10(np.loadtxt(cmpChargePath)) - norms[1][0]) / (norms[1][1]),
+            )
+            tmpList.append(join(self.target, deviceDir, cmpChargePath.split("/")[-1]))
 
-            np.savetxt(join(self.target, deviceDir, tarPotPath.split('/')[-1]), (np.loadtxt(tarPotPath) - norms[0][0]) / (norms[0][1]))
-            tmpList.append(join(self.target, deviceDir, tarPotPath.split('/')[-1]))
+            np.savetxt(
+                join(self.target, deviceDir, tarPotPath.split("/")[-1]),
+                (np.loadtxt(tarPotPath) - norms[0][0]) / (norms[0][1]),
+            )
+            tmpList.append(join(self.target, deviceDir, tarPotPath.split("/")[-1]))
 
-            np.savetxt(join(self.target, deviceDir, tarChargePath.split('/')[-1]), (np.log10(np.loadtxt(tarChargePath)) - norms[1][0]) / (norms[1][1]))
-            tmpList.append(join(self.target, deviceDir, tarChargePath.split('/')[-1]))
-            
+            np.savetxt(
+                join(self.target, deviceDir, tarChargePath.split("/")[-1]),
+                (np.log10(np.loadtxt(tarChargePath)) - norms[1][0]) / (norms[1][1]),
+            )
+            tmpList.append(join(self.target, deviceDir, tarChargePath.split("/")[-1]))
+
             tmpList.append(norms[0][0])
             tmpList.append(norms[0][1])
             tmpList.append(norms[1][0])
@@ -195,27 +224,31 @@ class Aggregator:
 
             dataList.append(tmpList)
 
-        self.df_cond = pd.DataFrame(dataList, columns=[
-        "Plane",
-        "VG",
-        "VD",
-        "Location",
-        "Criteria",
-        "Height",
-        "Width",
-        "inpPotPath",
-        "inpChargePath",
-        "cmpPotPath",
-        "cmpChargePath",
-        "tarPotPath",
-        "tarChargePath",
-        "Pot Mean",
-        "Pot STD",
-        "Charge Mean",
-        "Charge STD",
-    ],)
+        self.df_cond = pd.DataFrame(
+            dataList,
+            columns=[
+                "Plane",
+                "VG",
+                "VD",
+                "Location",
+                "Criteria",
+                "Height",
+                "Width",
+                "inpPotPath",
+                "inpChargePath",
+                "cmpPotPath",
+                "cmpChargePath",
+                "tarPotPath",
+                "tarChargePath",
+                "Pot Mean",
+                "Pot STD",
+                "Charge Mean",
+                "Charge STD",
+            ],
+        )
 
 
+# Parse args, read, save dataframe, condition, save conditioned.
 a1 = Aggregator(args)
 a1.read()
 a1.save("dataframe.csv")
--- data_loader.py
+++ data_loader.py
@@ -8,90 +8,98 @@ import numpy as np
 
 
 class NEFGSet(Dataset):
-    # This class should be used to create PyTorch compatible objects from a given data file
+    # This class should be used to create PyTorch compatible objects from a given dataframe
 
-    def __init__(
-        self, csv_file, root_dir, data_folder, shape=(111, 71), device="mps"
-    ):
-        # self.landmarks_frame = pd.read_csv(csv_file)
-        # self.root_dir = root_dir
-        self.shape = shape
-
-
-        # self.data_dir = os.path.join(root_dir, data_folder)
-        # self.labels = pd.read_csv(os.path.join(root_dir, csv_file), header=True)
-
-        self.df = pd.Dataframe()
+    def __init__(self, df, use_dimension=True, device="mps"):
+        self.df = df.reset_index()
         self.device = device
+        self.mode = use_dimension
 
     def __len__(self):
-        return len(self.labels)
+        return self.df.size
 
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
+        plane = self.df.iloc[idx]["Plane"]
+        shape = (0, 0)
+
+        x_val = ((20 + 2) * 5) + 1
+        y_val = (((self.df.iloc[idx]["Height"]) + 2) * 5) + 1
+        z_val = (((self.df.iloc[idx]["Width"]) + 2) * 5) + 1
+
+        if plane == "XY":
+            shape[0] = x_val
+            shape[1] = y_val
+        elif plane == "YZ":
+            shape[0] = y_val
+            shape[1] = z_val
+        elif plane == "ZX":
+            shape[0] = z_val
+            shape[1] = x_val
+
+        tmpList = []
         dat = []
-        imp = torch.stack(
+
+        tmpList.append(
+            torch.from_numpy(np.loadtxt(self.df.iloc[idx]["inpPotPath"])).to(
+                self.device
+            )
+        )  # Load POT File
+
+        tmpList.append(
+            torch.from_numpy(np.loadtxt(self.df.iloc[idx]["inpChargePath"])).to(
+                self.device
+            )
+        )  # Load CHARGE file
+
+        tmpList.append(torch.full(shape, self.df.iloc[idx]["VD"]).to(self.device))  # VD
+        tmpList.append(torch.full(shape, self.df.iloc[idx]["VG"]).to(self.device))  # VG
+        tmpList.append(
+            torch.full(shape, self.df.iloc[idx]["Location"]).to(self.device)
+        )  # Location
+
+        if self.mode:
+            tmpList.append(
+                torch.full(self.shape, self.df.iloc[idx]["Height"]).to(self.device)
+            )  # Add Height
+            tmpList.append(
+                torch.full(self.shape, self.df.iloc[idx]["Width"]).to(self.device)
+            )  # Add Width
+
+        tmpList.append(
             (
-                torch.from_numpy(
-                    np.loadtxt(
-                        self.df.iloc[idx]["inpPotPath"]
-                    )
-                ).to(self.device), # Load POT File
-
-                torch.from_numpy(
-                    np.loadtxt(
-                        self.df.iloc[idx]["inpChargePath"]
-                    )
-                ).to(self.device), # Load CHARGE file
-                
-                torch.full(self.shape, self.df.iloc[idx]["VD"]).to(self.device), # VD
-
-                torch.full(self.shape, self.df.iloc[idx]["VG"]).to(self.device), # VG
-
-                torch.full(self.shape, self.df.iloc[idx]["Location"]).to(self.device), # LOC
-
-                torch.full(self.shape, self.df.iloc[idx]["Height"]).to(self.device), # Add Height
-
-                torch.full(self.shape, self.df.iloc[idx]["Width"]).to(self.device), # Add Width
-
-                (
-                    torch.arange(0, self.shape[0])
-                    .reshape(self.shape[0], 1)
-                    .expand(self.shape[0], self.shape[1])
-                    / self.shape[0]
-                ).to(self.device),
-
-                (
-                    torch.arange(0, self.shape[1])
-                    .reshape(1, self.shape[1])
-                    .expand(self.shape[0], self.shape[1])
-                    / self.shape[1]
-                ).to(self.device),
-
-            ),
-
+                torch.arange(0, shape[0])
+                .reshape(shape[0], 1)
+                .expand(shape[0], shape[1])
+                / shape[0]
+            ).to(self.device)
         )
+        tmpList.append(
+            (
+                torch.arange(0, shape[1])
+                .reshape(1, shape[1])
+                .expand(shape[0], shape[1])
+                / shape[1]
+            ).to(self.device)
+        )
+
+        imp = torch.stack(tuple(tmpList), dim=0)
+
         dat.append(imp)
 
-        # self.labels.iloc[idx, 2] =  self.labels.iloc[idx, 2][:-5]+"3.txt"ß
-        # self.labels.iloc[idx, 3] =  self.labels.iloc[idx, 2][:-5]+"3.txt"
-
-        # print(self.labels.iloc[idx, 2])
-
         for i in range(8):
+            # Appends cmp and tar filepaths, as well as mean and standard deviation
             dat.append(
-                torch.from_numpy(
-                    np.loadtxt(
-                        self.df.iloc[idx, i + 9]
-                    )
-                ).to(self.device)
+                torch.from_numpy(np.loadtxt(self.df.iloc[idx, i + 9])).to(self.device)
             )
         for i in range(3):
+            # Appends VG, VD, Location
             dat.append(self.df.iloc[idx, i + 1])
 
         for i in range(2):
+            # Appends height and width
             dat.append(self.df.iloc[idx, i + 5])
 
         return dat
